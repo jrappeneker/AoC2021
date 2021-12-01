7bb2792e574732cdd6ee9c324ff900c9e65e38bc
Joshua Rappeneker <jrappeneker@gmail.com>
2021-12-02 08:44:00 +0900

    Reoganising

---
+++ AoC2021/Day.swift
@@ -0,0 +1,74 @@
+//
+//  Day.swift
+//  AoC2021
+//
+//  Created by Joshua Rappeneker on 2021/12/02.
+//
+
+import Foundation
+
+protocol Day {
+    associatedtype T : StringInitializable
+    
+    var day : Int { get }
+    func parse(filepath:String) -> [T]
+    func parseInt(filepath:String) -> [Int]
+    func run()
+    func partOne(_ input:[T]) -> Int
+    func partTwo(_ input:[T]) -> Int
+    @discardableResult func test() -> Bool
+    func log(_ message:String)
+}
+
+extension Day {
+    func log(_ message:String) {
+        print("day \(day):", message)
+    }
+    
+    func parseInt(filepath:String) -> [Int] {
+        let input = try! String(contentsOfFile: filepath)
+        return input.components(separatedBy: .newlines).compactMap(Int.init)
+    }
+    
+    func parse(filepath:String) -> [T] {
+        let input = try! String(contentsOfFile: filepath)
+        return input.components(separatedBy: .newlines).compactMap(T.init)
+    }
+    
+    @discardableResult func test() -> Bool {
+        log("running tests...")
+
+        let folders = try! FileManager.default.contentsOfDirectory(atPath: "tests/\(day)")
+    
+        var failed = false
+        
+        folders.forEach { f in
+            let folderPath = "tests/\(day)/\(f)/"
+            
+            let input : [T] = parse(filepath: folderPath+"input")
+            let answers : [Int] = parseInt(filepath: folderPath+"answer")
+            
+            if partOne(input) != answers[0] {
+                log("test \(f) part 1 FAILED")
+                failed = true
+            } else if answers.count > 1 && partTwo(input) != answers[1] {
+                log("test \(f) part 2 FAILED")
+                failed = true
+            }
+            
+        }
+        if !failed {
+            log("all tests passed")
+        } else {
+            log("tests complete")
+        }
+        return true
+    }
+
+}
+
+protocol StringInitializable {
+    init?(_ s:String)
+}
+
+extension Int : StringInitializable {}
--- AoC2021/main.swift
+++ AoC2021/main.swift
@@ -7,35 +7,6 @@
 
 import Foundation
 
-let input =
-"""
-1
-2
-3
-"""
-
-let numbers = input.components(separatedBy: .newlines).compactMap(Int.init)
-
-var current = numbers[0]
-var count = 0
-for n in numbers {
-    if n > current {
-        count += 1
-    }
-    current = n
-}
-
-
-print(count)
-
-count = 0
-current = numbers[0..<3].reduce(0, +)
-for i in 3..<numbers.count {
-    let sum = numbers[i-2...i].reduce(0, +)
-    if sum > current {
-        count += 1
-    }
-    current = sum
-}
-
-print(count)
+let d = Day1()
+d.test()
+d.run()
