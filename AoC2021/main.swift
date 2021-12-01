a11abf40b2099d3c457ec85148fb96ac988c5060
Joshua Rappeneker <jrappeneker@gmail.com>
2021-12-01 14:34:57 +0900

    Setup inputs and tests

--- AoC2021/main.swift
+++ AoC2021/main.swift
@@ -7,5 +7,35 @@
 
 import Foundation
 
-print("Hello, World!")
+let input =
+"""
+1
+2
+3
+"""
 
+let numbers = input.components(separatedBy: .newlines).compactMap(Int.init)
+
+var current = numbers[0]
+var count = 0
+for n in numbers {
+    if n > current {
+        count += 1
+    }
+    current = n
+}
+
+
+print(count)
+
+count = 0
+current = numbers[0..<3].reduce(0, +)
+for i in 3..<numbers.count {
+    let sum = numbers[i-2...i].reduce(0, +)
+    if sum > current {
+        count += 1
+    }
+    current = sum
+}
+
+print(count)
